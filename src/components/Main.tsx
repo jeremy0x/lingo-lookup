299ae7923894c07ad3459477a6f277ee57aee3c5
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2023-04-07 05:58:11 +0100

    fix: increase margin bottom for footer

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -44,7 +44,7 @@ const Main = () => {
   }
 
   return (
-    <main className='mb-20 flex-grow'>
+    <main className='mb-32 flex-grow'>
       {/* search form */}
       <form
         className='animate__animated animate__fadeIn mx-auto w-full max-w-5xl'
