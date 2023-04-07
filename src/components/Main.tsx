72494f9f4002c82f3911a523179501b212fc3144
jeremy0x <aworetanjeremiah@gmail.com>
2023-04-07 22:44:20 +0100

    style: reduce form height

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -71,7 +71,7 @@ const Main = () => {
           <input
             type='search'
             id='search'
-            className='block w-full rounded-lg border border-gray-600 bg-gray-700 p-4 pl-10 text-base text-gray-300 placeholder-gray-400 transition-all focus:border-blue-500 focus:outline-none focus:ring-blue-500'
+            className='block w-full rounded-lg border border-gray-600 bg-gray-700 p-3 pl-10 text-base text-gray-300 placeholder-gray-400 transition-all focus:border-blue-500 focus:outline-none focus:ring-blue-500'
             placeholder='Search for a word...'
             required
             value={searchQuery}
@@ -80,7 +80,7 @@ const Main = () => {
           />
           <button
             type='submit'
-            className='absolute right-2.5 bottom-2.5 rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-800'
+            className='absolute right-2.5 bottom-2 rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-800'
           >
             Search
           </button>
