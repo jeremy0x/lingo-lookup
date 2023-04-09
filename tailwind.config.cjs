b4332a2ba574d4c526947351129323696fe7c2c8
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2023-04-09 21:48:49 +0100

    fix: define custom property for 3.4rem

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        3.4: '3.4rem',
+      },
+    },
   },
   plugins: [],
 };
