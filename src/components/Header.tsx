dbb9d5857968708186bd0b32759bc9e89bf76de7
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2023-04-10 05:12:24 +0100

    style: truncate header text on small screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import 'animate.css';
 const Header = () => {
   return (
     <header className='mt-16 mb-10 flex flex-col place-items-center gap-8 text-center'>
-      <h1 className='animate__animated animate__bounceInDown text-5xl font-semibold sm:text-6xl'>
+      <h1 className='animate__animated animate__bounceInDown max-w-full overflow-hidden truncate whitespace-nowrap px-4 text-5xl font-semibold sm:text-6xl'>
         LingoLookup
       </h1>
-      <h3 className='animate__animated animate__fadeIn text-lg font-light text-gray-400 sm:text-2xl'>
+      <h3 className='animate__animated animate__fadeIn px-4 text-lg font-light text-gray-400 sm:text-2xl'>
         Explore word definitions, antonyms, synonyms, origins, and pronunciation
         in seconds!
       </h3>
