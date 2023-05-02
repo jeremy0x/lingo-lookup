7a1c61c4c7aa70296609af03cf02ac3b5803c5ad
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2023-05-02 14:23:46 +0100

    style: change background gradient

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Main from './components/Main';
 
 function App() {
   return (
-    <div className='relative flex min-h-screen flex-col0 bg-gradient-to-br from-gray-900 to-gray-800 text-white'>
+    <div className='relative flex min-h-screen flex-col0 bg-gradient-to-br from-gray-800 to-gray-900 text-white'>
       <div className='container mx-auto'>
         <Header />
         <Main />
