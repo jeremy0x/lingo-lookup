8f3c65eb41fcf33e3beb34baf69cd0953dddeece
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2023-05-04 14:04:00 +0100

    style: change header text font size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import 'animate.css';
 const Header = () => {
   return (
     <header className='mt-16 mb-10 flex flex-col place-items-center gap-4 text-center'>
-      <h1 className='animate__animated animate__bounceInDown max-w-full overflow-hidden truncate whitespace-nowrap bg-gradient-to-r from-blue-600 to-white bg-clip-text px-4 pb-4 text-5xl font-extrabold text-transparent sm:text-6xl'>
+      <h1 className='animate__animated animate__bounceInDown max-w-full overflow-hidden truncate whitespace-nowrap bg-gradient-to-r from-blue-600 to-white bg-clip-text px-4 pb-4 text-5xl font-extrabold text-transparent sm:text-6xl xl:text-7xl'>
         LingoLookup
       </h1>
 
