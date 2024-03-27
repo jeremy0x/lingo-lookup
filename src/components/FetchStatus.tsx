a4d97ba8f90d98b1b1bb2d8a5f11626127453349
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2024-03-27 06:17:04 +0100

    fix: update error message in FetchStatus component

--- src/components/FetchStatus.tsx
+++ src/components/FetchStatus.tsx
@@ -6,7 +6,7 @@ export const ErrorStatus = () => {
   return (
     <div className='animate__animated animate__fadeIn mt-8 flex flex-col items-center justify-center text-xl text-gray-400'>
       <img src={errorAnimation} alt='error animation' className='w-36' />
-      <p>There was an error...</p>
+      <p>There was an error... Please try again later.</p>
     </div>
   );
 };
